eb970d6cd0da72432099810ead94815288d848ea
Bastien <stanbyes@gmail.com>
2021-01-09 23:50:51 +0100

    Update import and pass on PHP code

--- index.php
+++ index.php
@@ -5,14 +5,14 @@
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
     <title>Welcome - RadBOT</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <link rel='stylesheet' type='text/css' media='screen' href='./css/style.css'>
-    <script src='./js/main.js'></script>
+    <link rel='stylesheet' type='text/css' media='screen' href='./assets/css/style.css'>
+    <script src='./assets/js/main.js'></script>
 </head>
 <body>
     <!--Welcome-->
     <div class="main" id="welcome">
         <h1 class="main" id="h-main">Welcome On RadBot DashBoard</h1>
-        <p class="main" id="p-desc"></p>>
+        <p class="main" id="p-desc">What is <strong>RadBot</strong> ? RadBot is a Discord Bot focused on Music and Moderation</p>>
     </div>
 </body>
 </html>
